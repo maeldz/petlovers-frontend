f71637e3f3f7d455482f0985b461f531efb7089d
Ismael de Jesus <mael.dazareia@gmail.com>
2021-05-08 09:35:28 -0300

    chore: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div>
       <p>Hello World</p>
     </div>
   );
-}
+};
 
 export default App;
